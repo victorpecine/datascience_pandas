77828e9bbfe3ed31610684a740002ea8e04cc019
victorpecine <victorpecine@gmail.com>
2022-08-23 05:14:28 -0300

    análise tipos de imóveis

--- aluguel.py
+++ aluguel.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-dataset = pd.read_csv('aluguel.csv', delimiter=';')
+dataset = pd.read_csv('aluguel.csv', sep=';')
 
 # Informações sobre as base de dados
 # print(dataset.dtypes)
@@ -12,3 +12,18 @@ tipos_dados.columns.name = 'Variáveis'
 # Qtd. de registros e variáveis
 linhas_colunas = dataset.shape
 # print('A base de dados apresenta {} registros e {} variáveis'.format(linhas_colunas[0], linhas_colunas[1]))
+
+
+# Tipos de imóveis
+tipos_imoveis = dataset['Tipo']
+tipos_imoveis.drop_duplicates(inplace=True)
+tipos_imoveis = pd.DataFrame(tipos_imoveis)
+
+# print(tipos_imoveis)
+
+tamanho_dataframe_tipos_imoveis = tipos_imoveis.shape[0]
+# print(tamanho_dataframe_tipos_imoveis)
+
+tipos_imoveis.index = range(tipos_imoveis.shape[0])
+tipos_imoveis.columns.name = 'idTipo'
+print(tipos_imoveis)
