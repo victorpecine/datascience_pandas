d203eef2195ef591f44097cbfdba9505c1429199
victorpecine <victorpecine@gmail.com>
2022-08-23 03:22:21 -0300

    aula_2

--- carros.py
+++ carros.py
@@ -5,8 +5,29 @@ import pandas as pd
 
 dataset = pd.read_csv('db.csv', sep = ';')
 
-print(dataset)
+# print(dataset)
 
 # Conjunto de estatíticas descritivas
 estat_carros = dataset[['Quilometragem', 'Valor']].describe()
-print(estat_carros)
+# print(estat_carros)
+
+
+dados = {
+    'Crossfox': {'km': 35000, 'ano': 2005}, 
+    'DS5': {'km': 17000, 'ano': 2015}, 
+    'Fusca': {'km': 130000, 'ano': 1979}, 
+    'Jetta': {'km': 56000, 'ano': 2011}, 
+    'Passat': {'km': 62000, 'ano': 1999}
+}
+
+def km_media(dataset, ano_atual):
+    result = {}
+    for item in dataset.items():
+        media = item[1]['km'] / (ano_atual - item[1]['ano'])
+        item[1].update({ 'km_media': media })
+        result.update({ item[0]: item[1] })
+
+    return result
+
+carros = pd.DataFrame(km_media(dados, 2019)).T
+print(carros)
