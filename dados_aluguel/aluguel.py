fc2db5b19cd43673408b98f7622cab6c7b140c5d
victorpecine <victorpecine@gmail.com>
2022-08-26 12:59:24 -0300

    ajuste de pastas

---
+++ dados_aluguel/aluguel.py
@@ -0,0 +1,158 @@
+from turtle import color
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+dataset = pd.read_csv('aluguel.csv', sep=';')
+
+# Informações sobre as base de dados
+# print(dataset.dtypes)
+# print(dataset)
+
+# tipos_dados = pd.DataFrame(dataset.dtypes, columns=['Tipos de dados'])
+# tipos_dados.columns.name = 'Variáveis'
+# print(tipos_dados)
+
+# Qtd. de registros e variáveis
+linhas_colunas = dataset.shape
+# print('A base de dados apresenta {} registros e {} variáveis'.format(linhas_colunas[0], linhas_colunas[1]))
+
+
+# Tipos de imóveis
+tipos_imoveis = dataset['Tipo']
+tipos_imoveis.drop_duplicates(inplace=True)
+tipos_imoveis = pd.DataFrame(tipos_imoveis)
+
+# print(tipos_imoveis)
+
+tamanho_dataframe_tipos_imoveis = tipos_imoveis.shape[0]
+# print(tamanho_dataframe_tipos_imoveis)
+
+tipos_imoveis.index = range(tipos_imoveis.shape[0])
+tipos_imoveis.columns.name = 'idTipo'
+# print(tipos_imoveis)
+
+# Identificando imóveis residenciais
+residencial = ['Quitinete',
+                'Casa',
+                'Flat',
+                'Apartamento',
+                'Casa de Condomínio',
+                'Casa de Vila',
+                'Studio']
+
+# Selecionando apenas os tipos residenciais
+series_residencial = dataset['Tipo'].isin(residencial)
+
+# Dataframe tipo residencial
+dados_residencial = dataset[series_residencial]
+dados_residencial.index = range(dados_residencial.shape[0])
+# print(dados_residencial)
+
+# Exportando a base de dados
+# dados_residencial.to_csv('dados_residencial.csv', sep=';', index=False)
+
+
+# Selecione somente os imóveis classificados com tipo 'Apartamento'
+# df = dataframe
+series_apartamentos = dados_residencial['Tipo'] == 'Apartamento'
+
+df_apartamentos = dados_residencial[series_apartamentos].shape[0]
+# print('> {} apartamentos'.format(df_apartamentos))
+
+
+# Selecione os imóveis classificados com tipos 'Casa', 'Casa de Condomínio' e 'Casa de Vila'
+series_casas = (dados_residencial['Tipo'] == 'Casa') | (dados_residencial['Tipo'] == 'Casa de Condomínio') | (dados_residencial['Tipo'] == 'Casa de Vila')
+
+df_casas = dados_residencial[series_casas].shape[0]
+# print('> {} casas'.format(df_casas))
+
+
+# Selecione os imóveis com área entre 60 e 100 metros quadrados, incluindo os limites
+series_areas = (dados_residencial['Area'] >= 60) & (dados_residencial['Area'] <= 100)
+
+df_areas = dados_residencial[series_areas].shape[0]
+# print('> {} imóveis com área entre 60 m² e 100 m²'.format(df_areas))
+
+
+# Selecione os imóveis que tenham pelo menos 4 quartos e aluguel menor que R$ 2.000,00
+series_quartos_alugueis = (dados_residencial['Quartos'] >= 4) & (dados_residencial['Valor'] < 2000)
+
+df_quartos_alugueis = dados_residencial[series_quartos_alugueis].shape[0]
+# print('> {} imóveis com, no mínimo, 4 quartos e aluguel abaixo de R$ 2.000,00'.format(df_quartos_alugueis))
+
+# Lista dos imóveis que tenham pelo menos 4 quartos e aluguel menor que R$ 2.000,00
+# print(dados_residencial[series_quartos_alugueis])
+
+
+# Identificando dados nulos
+# print(dataset.info())
+
+# Visualizando registros nulos
+dataset_valores_nulos = (dataset['Valor'].isnull()) | (dataset['Condominio'].isnull()) | (dataset['IPTU'].isnull())
+# print(dataset[dataset_valores_nulos])
+
+
+# Alterando valores nulos para 0
+dataset = dataset.fillna({'Valor':0, 'Condominio': 0, 'IPTU': 0})
+# print(dataset)
+
+# dataset.to_csv('aluguel.csv', sep=';', index=False)
+# dados_residencial.to_csv('dados_residencial.csv', sep=';', index=False)
+
+
+# Criando novas variáveis
+dados_residencial['Valor bruto'] = dados_residencial['Valor'] + dados_residencial['Condominio'] + dados_residencial['IPTU']
+
+dados_residencial['Valor/m2'] = (dados_residencial['Valor'] / dados_residencial['Area']).round(2)
+
+# Excluindo registros com Valor = 0
+dados_residencial.drop(dados_residencial[dados_residencial['Valor'] == 0].index, inplace=True)
+
+# print(dados_residencial.sort_values(by=['Valor/m2'], ascending=False))
+
+
+# Agrupando tipos entre Casa ou Apartamento
+casa = ['Casa', 'Casa de Condomínio', 'Casa de Vila']
+dados_residencial['Tipo agregado'] = dados_residencial['Tipo'].apply(lambda x: 'Casa' if x in casa else 'Apartamento')
+
+# print(dados_residencial)
+
+del dados_residencial['Valor bruto']
+
+# print(dados_residencial)
+
+# dados_residencial.to_csv('dados_residencial.csv', sep=';', index=False)
+
+# Contagem de tipos
+
+# print('*'*30)
+
+cont_tipos_dataset = dataset['Tipo'].value_counts()
+# print(cont_tipos_dataset)
+
+# print('*'*30)
+
+cont_tipos_residencial = dados_residencial['Tipo'].value_counts()
+# print(cont_tipos_residencial)
+
+# print('*'*30)
+
+bairros = dados_residencial.groupby('Bairro')
+media_preco_bairros = bairros[['Valor', 'Condominio']].mean().round(2)
+
+print(media_preco_bairros.sort_values(by=['Valor'], ascending=False))
+
+# fig_std = bairros['Valor'].std().plot.bar(color = 'blue')
+# fig_std.set_ylabel('R$')
+# fig_std.set_title('Desvio padrão do aluguel por bairro')
+# plt.show()
+
+# fig_mean = bairros['Valor'].mean().plot.bar(color = 'green')
+# fig_mean.set_ylabel('R$')
+# fig_mean.set_title('Valor médio do aluguel por bairro')
+# plt.show()
+
+# Identifica outliers com boxplot
+fig = dataset.boxplot(['Valor'])
+plt.show()
---
+++ dados_escola/escola.py
@@ -0,0 +1,114 @@
+import pandas as pd
+import numpy as np
+import html5lib
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sqlalchemy import create_engine, MetaData, Table, inspect
+
+nomes_f = pd.read_json('https://guilhermeonrails.github.io/nomes_ibge/nomes-f.json')
+
+nomes_m = pd.read_json('https://guilhermeonrails.github.io/nomes_ibge/nomes-m.json')
+
+# Agrupa os dataframes
+frames = [nomes_f, nomes_m]
+df_alunos = pd.concat(frames)['nome'].to_frame()
+
+total_alunos = len(df_alunos)
+
+# Cria ids aleatórios entre 1 e 400
+np.random.seed(123)
+df_alunos['id_aluno'] = np.random.permutation(total_alunos) + 1 # Soma 1 para não ter id 0
+
+
+# Cria e-mail par cada aluno a partir dos domínios
+dominios = ['@gmail.com', '@yahoo.com', '@outlook.com', '@terra.com.br', '@hotmail.com']
+df_alunos['dominio_email'] = np.random.choice(dominios, total_alunos) # Atribui os domínios de forma aleatória
+
+df_alunos['email'] = df_alunos.nome.str.cat(df_alunos.dominio_email).str.lower()
+
+
+# Exporta csv com a lista dos alunos
+# nomes.to_csv('nomes_alunos.csv', sep=';', index=False)
+
+
+url = 'https://tabela-cursos.herokuapp.com/index.html'
+lista_cursos = pd.read_html(url)
+
+df_cursos = lista_cursos[0] # Cria dataframe de cursos
+
+df_cursos = df_cursos.rename(columns={'Nome do curso': 'nome_do_curso'})
+
+df_cursos['id_curso'] = df_cursos.index + 1
+
+df_cursos = df_cursos.set_index('id_curso')
+
+
+
+df_alunos['qtd_matriculas'] = np.ceil(np.random.exponential(size=total_alunos) * 1.5).astype(int) # Cria a quantidade de matrículas para cada aluno
+
+# graf_matriculas = sns.displot(df_alunos['qtd_matriculas'])
+# graf_matriculas.set(xlabel='Qtd. de matrículas', ylabel='Qtd. de alunos')
+# plt.show()
+
+# qtd_matriculas_alunos = df_alunos['qtd_matriculas'].value_counts()
+
+
+# Atribui cursos aos alunos
+matriculas_aluno = []
+x = np.random.rand(df_cursos.shape[0])
+prob = x / sum(x)
+
+for index, row in df_alunos.iterrows():
+    id = row.id_aluno
+    matriculas = row.qtd_matriculas
+    cont = 0
+    cursos_aluno = []
+    while cont < matriculas:
+        curso = np.random.choice(df_cursos.index, p=prob)
+        if(curso not in cursos_aluno):
+            matriculas_aluno.append([id, curso])
+            cursos_aluno.append(curso)
+            cont += 1
+
+df_matriculas_alunos = pd.DataFrame(matriculas_aluno, columns=['id_aluno', 'id_curso'])
+# df_matriculas_alunos.to_csv('matriculas_alunos.csv', sep=';', index=False)
+
+df_matriculas_cursos = df_matriculas_alunos.groupby('id_curso').count().join(df_cursos['nome_do_curso']).rename(columns={'id_aluno': 'qtd_de_alunos'})
+# df_matriculas_cursos.to_csv('matriculas_cursos.csv', sep=';', index=False)
+
+
+# Cria o banco SQL
+engine = create_engine('sqlite:///:memory:') # SQLite salvando na memória local
+
+df_matriculas_cursos.to_sql('matriculas_cursos', engine) # Cria a tabela matriculas_cursos no banco
+
+df_alunos.to_sql('alunos', engine)
+
+df_cursos.to_sql('cursos', engine)
+
+inspector = inspect(engine) # Cria um inspector object para o banco
+
+query_1= 'select * from matriculas_cursos where nome_do_curso like "%mysql%"'
+query_1r = pd.read_sql(query_1, con=engine) # r = read
+
+query_matriculas = pd.read_sql_table('matriculas_cursos', engine, columns=['nome_do_curso', 'qtd_de_alunos'])
+query_matriculasr = query_matriculas.query('nome_do_curso.str.contains("MySql")')
+
+# print(inspector.get_table_names())
+
+query_2= 'select * from alunos where qtd_matriculas >= 9'
+query_2r = pd.read_sql(query_2, con=engine)
+
+
+# Cria e exporta as listas de alunos por curso
+# lista_id_cursos = list(df_cursos.index)
+
+
+for id_curso, row in df_cursos.iterrows():
+    nome_curso = row['nome_do_curso']
+
+    lista_alunos_curso = df_matriculas_alunos.query('id_curso == {}'.format(id_curso))
+    lista_alunos_curso = lista_alunos_curso.set_index('id_aluno').join(df_alunos.set_index('id_aluno'))['nome'].to_frame()
+
+    lista_alunos_curso = lista_alunos_curso.rename(columns={'nome': 'Alunos do curso de {}'.format(nome_curso)})
+    lista_alunos_curso.to_csv('lista_cursos_alunos/lista_alunos_curso_{}.csv'.format(nome_curso).lower(), sep=';')
